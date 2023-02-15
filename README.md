9b3ccfc30405142f9b2bfdd63ea04f8828aceea1
Hannes Schneider <hannes.schneider@tu-braunschweig.de>
2023-02-15 10:42:42 +0100

    change quantiles

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Visualize uncertainty with sankey diagrams.
 
 
-This is a python tool written for the depart de sentier [Sustainability assessment visualization contest](https://github.com/Depart-de-Sentier/visualization-contest-2022) to visualize brightway2 Monte Carlo LCA results with [plotly sankey diagrams](https://plotly.com/python/sankey-diagram/). sankertainpy is using an adjusted version of the [bw2analyzer function "recursive_calculation_to_object()"](https://github.com/brightway-lca/brightway2-analyzer/blob/main/bw2analyzer/utils.py) to generate the plotly compatible graph traversal LCA data. With the function generate_sankey(), direct emission impact can be calculated, flows smaller than the cutoff value are bundled to single nodes, and the uncertainty visualisation is generated. It is tested with brightway2.5 and ecoinvent 3.8 cut-off.  
+This is a python package written for the depart de sentier [Sustainability assessment visualization contest](https://github.com/Depart-de-Sentier/visualization-contest-2022) to visualize brightway2 Monte Carlo LCA results with [plotly sankey diagrams](https://plotly.com/python/sankey-diagram/). sankertainpy is using an adjusted version of the [bw2analyzer function "recursive_calculation_to_object()"](https://github.com/brightway-lca/brightway2-analyzer/blob/main/bw2analyzer/utils.py) to generate the plotly compatible graph traversal LCA data. With the function generate_sankey(), direct emission impact can be calculated, flows smaller than the cutoff value are bundled to single nodes, and the uncertainty visualisation is generated. It is tested with brightway2 and ecoinvent 3.8 cut-off.  
 
 
 ## Getting started:
@@ -21,11 +21,11 @@ method= bd.methods.random()
 
 result = recursive_calculation_to_plotly(activity,method)
 
-fig= sp.generate_sankey(result, type=1, cutoff=0.04)
+fig= sp.generate_sankey(result, type=1)
 
 fig.show()
 ```
-
+Note: depending on the number of monte carlo iterations and the level depth, the calculation can take some time!
 
 ## Background:
 
--- sankertainpy/sankertainpy.py
+++ sankertainpy/sankertainpy.py
@@ -2,114 +2,117 @@ import numpy as np
 import plotly.graph_objects as go
 import matplotlib
 
-def cut_off_flows(result, label_list, cutoff):
+def cut_off_flows(data, label_list, cutoff):
     # Combine flows < cutoff to one node:
-    total_score= np.mean(result['scores'][0])
+    total_score= np.mean(data['scores'][0])
     label_list.append('Activities< cutoff')
     upstream=len(label_list)-1
     label_list.append('Activities< cutoff downstream')
     downstream=len(label_list)-1
 
-    for i, flow in enumerate(result['scores']):
+    for i, flow in enumerate(data['scores']):
         if abs(np.mean(flow))/abs(total_score) < cutoff:  
             if np.mean(flow) > 0:
-                result['sources'][i]= upstream
+                data['sources'][i]= upstream
             else:
-                result['sources'][i]= downstream
-    return(result, label_list)
+                data['sources'][i]= downstream
+    return(data, label_list)
 
 
-def add_emissions(i, result, label_list, upstream, downstream):
+def add_emissions(i, data, label_list, upstream, downstream):
     # 
     inp=0
     out=0
-    for n, scr in enumerate(result['scores']):
-        if result['sources'][n]== i:
+    for n, scr in enumerate(data['scores']):
+        if data['sources'][n]== i:
             out= out + np.mean(scr)
             
-        if result['targets'][n]== i:
+        if data['targets'][n]== i:
             inp= inp+ np.mean(scr)
 
     if inp!=0 and out!= 0 and inp != out:
         
         if out > 0:
-            result['scores'].append(out-inp)
-            result['sources'].append(upstream)
-            result['targets'].append(i)
+            data['scores'].append(out-inp)
+            data['sources'].append(upstream)
+            data['targets'].append(i)
         else:
-            result['scores'].append(out-inp)
-            result['sources'].append(downstream)
-            result['targets'].append(i)
+            data['scores'].append(out-inp)
+            data['sources'].append(downstream)
+            data['targets'].append(i)
 
 
-    return(result, label_list)
+    return(data, label_list)
 
 
-def calc_emissions(result, label_list):
+def calc_emissions(data, label_list):
     # iterate through nodes and link emissions to aditional node
     label_list.append('Emissions')
     upstream=len(label_list)-1
     label_list.append('Emissions downstream')
     downstream = len(label_list)-1
-    result['nodes'][len(label_list)-1]= {'name':'Emissions'}
+    data['nodes'][len(label_list)-1]= {'name':'Emissions'}
     
-    for _, (i, _) in enumerate(result['nodes'].items()):
-        result,label_list= add_emissions(i, result, label_list, upstream, downstream)
+    for _, (i, _) in enumerate(data['nodes'].items()):
+        data,label_list= add_emissions(i, data, label_list, upstream, downstream)
     
-    return(result, label_list)
+    return(data, label_list)
 
-def calc_quantile_flows(result, cutoff):
-    # Split flows with list of Monte Carlo results into different quantiles
-    total_score= np.mean(result['scores'][0])
+def calc_quantile_flows(data, cutoff):
+    # Split flows with list of Monte Carlo datas into different quantiles
+    total_score= np.mean(data['scores'][0])
     cmap = matplotlib.cm.get_cmap('RdYlGn')
     cmap_basic = matplotlib.cm.get_cmap('Blues')
-    quantiles= np.arange(0.1,0.9,0.1) 
+    quantiles= np.arange(0.125,0.925,0.05) 
     #neg_quantiles= np.arange(0.9,0.9,0.1)
     new_targets, new_sources, new_scores, colors=[], [], [], []    
-
-    for i, flow in enumerate(result['scores']):
+    hoverlabel=[]
+    for i, flow in enumerate(data['scores']):
         if type(flow) == list and abs(np.mean(flow)/total_score) > cutoff:           
             if np.mean(flow) <= 0:
                 act_quantiles=np.flip(quantiles)
             else:
                 act_quantiles=quantiles
 
-            old_qu_score = 0 #np.quantile(flow, 0.1)
+            old_qu_score = 0 
             for n, qu in enumerate(act_quantiles):
                 
                 new_scores.append(np.quantile(flow, qu)- old_qu_score)
-                new_targets.append(result['targets'][i])
-                new_sources.append(result['sources'][i])
+                new_targets.append(data['targets'][i])
+                new_sources.append(data['sources'][i])
                 if old_qu_score == 0:
                     colors.append('rgba'+str(cmap_basic(0.5,0.6)))
+                    hoverlabel.append('score < 0.125 quantile')
                 else:
                     if np.mean(flow) <= 0:
-                        colors.append('rgba'+str(cmap(qu,0.9)))
+                        colors.append('rgba'+str(cmap(qu-0.025,0.9)))
                     else:
                         colors.append('rgba'+str(cmap(1-qu,0.9)))
+                    hoverlabel.append(f'score between {round(qu-0.05,4)} quantile and {round(qu,4)} quantile ({round(np.quantile(flow, qu),5)})')
                 
                 old_qu_score=np.quantile(flow, qu)
 
-        else: #Add flows without Monte Carlo results
+        else: #Add flows without Monte Carlo datas
             new_scores.append(np.mean(flow))
-            new_targets.append(result['targets'][i])
-            new_sources.append(result['sources'][i])
+            new_targets.append(data['targets'][i])
+            new_sources.append(data['sources'][i])
             colors.append('rgba'+str(cmap_basic(0.5,0.6)))
-    new_result={'targets':new_targets, 'sources':new_sources, 'scores':new_scores, 'nodes':result['nodes'],'colors':colors}
-    return(new_result)
+            hoverlabel.append(f'score without MonteCarlo calculation ({round(np.quantile(flow, qu),4)})')
+    new_data={'targets':new_targets, 'sources':new_sources, 'scores':new_scores, 'nodes':data['nodes'],'colors':colors}
+    return(new_data, hoverlabel)
     
 
-def calc_colors(result, cutoff):
+def calc_colors(data, cutoff):
     #
-    total_score= np.mean(result['scores'][0])
+    total_score= np.mean(data['scores'][0])
     cmap_mc = matplotlib.cm.get_cmap('YlOrRd')
     cmap_smc = matplotlib.cm.get_cmap('Blues')
     
-    max_std= max([np.std(fl) for fl in result['scores']])
+    max_std= max([np.std(fl) for fl in data['scores']])
     
-    result['colors']=[0]*len(result['scores'])
-    
-    for i, flow in enumerate(result['scores']):
+    data['colors']=[0]*len(data['scores'])
+    hoverlabel=[0]*len(data['scores'])
+    for i, flow in enumerate(data['scores']):
         if type(flow) == list and np.mean(flow)/total_score > cutoff:    
             scale=  round(np.std(flow)/max_std, 2) 
             color= cmap_mc(scale,0.9)
@@ -124,45 +127,47 @@ def calc_colors(result, cutoff):
             new_color.append(0.9)
             color=tuple(new_color)
 
-        else: #Add flows without Monte Carlo results
+        else: #Add flows without Monte Carlo datas
             color=cmap_smc(0.5,0.6)
+        hoverlabel[i]=f'score: {round(np.mean(flow),5)}; std: {round(np.std(flow),5)})'
+        data['colors'][i]='rgba'+str(color)
+        data['scores'][i] = np.mean(flow)
+    return(data, hoverlabel)
 
-        result['colors'][i]='rgba'+str(color)
-        result['scores'][i] = np.mean(flow)
-    return(result)
-
-def flip_negativ_values(result):    
-    for i, flow in enumerate(result['scores']):
+def flip_negativ_values(data):    
+    for i, flow in enumerate(data['scores']):
         if flow <= 0:
-            #p= result['targets'][i]
-            #result['targets'][i]= result['sources'][i]
-            #result['sources'][i]= p
-            result['scores'][i]=abs(flow)
+            #p= data['targets'][i]
+            #data['targets'][i]= data['sources'][i]
+            #data['sources'][i]= p
+            data['scores'][i]=abs(flow)
 
-    return(result)
+    return(data)
 
-def adjust_result(result, type, cutoff, emission):
-    label_list=[result['nodes'][nod]['name'] for nod in result['nodes']]
+def adjust_data(data, type, cutoff, emission):
     
-    result, label_list = cut_off_flows(result, label_list, cutoff)
+    
+    label_list=[data['nodes'][nod]['name'] for nod in data['nodes']]
+    
+    data, label_list = cut_off_flows(data, label_list, cutoff)
     if emission:
-        result, label_list = calc_emissions(result, label_list)
+        data, label_list = calc_emissions(data, label_list)
     
     if type ==1:
-        result= calc_quantile_flows(result, cutoff)
+        data, hoverlabel= calc_quantile_flows(data, cutoff)
     elif type ==0:
-        result= calc_colors(result, cutoff)
+        data, hoverlabel= calc_colors(data, cutoff)
     
     #flip negative values:
-    result= flip_negativ_values(result)
-    return(result, label_list)
+    data= flip_negativ_values(data)
+    return(data, label_list, hoverlabel)
 
-def generate_sankey(result, type=1, cutoff= 0.05, emissions= True):
+def generate_sankey(data, type=1, cutoff= 0.05, emissions= True, method= ''):
     '''
     Generate a plotly sankey figure with uncertainty. Made for visualizing LCA data from brightway2 activities. 
     
     Args:
-        result= {
+        data= {
             'sources': list of int regarding the source nodes from 'nodes'
             'targets': list of int regarding the target nodes from 'nodes'
             'scores': list of floats/list containing the weight of the links between the nodes from 'nodes'. Monte Carlo results are wrapped in a nested list.
@@ -180,7 +185,9 @@ def generate_sankey(result, type=1, cutoff= 0.05, emissions= True):
 
 
     '''
-    result, label_list = adjust_result(result, type, cutoff, emissions)    
+    data['metadata']= {'method':method, 'activity':data['nodes'][0]['name']}
+
+    data, label_list, hoverlabel = adjust_data(data, type, cutoff, emissions)    
 
     # style the figure:
     node_color=['grey']*len(label_list)
@@ -196,15 +203,19 @@ def generate_sankey(result, type=1, cutoff= 0.05, emissions= True):
       line = dict(color = "black", width = 0.0),
       label = label_list,
       color = node_color,
+      customdata = label_list,
+      hovertemplate='%{customdata}',
     ),
     link = dict(
-      source = result['sources'],
-      target = result['targets'],
-      value = result['scores'],
-      color= result['colors'],
+      source = data['sources'],
+      target = data['targets'],
+      value = data['scores'],
+      color= data['colors'],
       #arrowlen=10,
+      customdata =hoverlabel,
+      hovertemplate= '%{customdata}',
     ))])
     fig.update_layout(width=1400)
     fig.update_layout(height=800)
-    #fig.update_layout(title_text="Basic Sankey Diagram", font_size=12)
+    fig.update_layout(title_text= data['metadata']['name'] + ' - ' + data['metadata']['method'], font_size=12)
     return(fig)
